c510386318ac861e9d184fc5e56c9e50143d7d4f
Lucas Costa E Silva Almeida <LUCASC.COMPASSO@fleetcor.com.br>
2025-02-10 21:26:33 -0300

    feature: fix app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ComicsService } from '../api/getComics';
+import { ListItemsModel } from '../components/ListItems/Models/ListItemsModel';
 import { RouterOutlet } from '@angular/router';
+import { map, Observable } from 'rxjs';
+import { selectComics } from '../store/selectors/comics';
+import { AppState } from '../store/app.state';
+import { Store } from '@ngrx/store';
+import { loadComicsSuccess } from '../store/actions/comics';
+import { ComicsState } from '../store/reducers/comics';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'avengers';
+export class AppComponent implements OnInit {
+  items$: Observable<any>;
+
+  constructor(private comicsService: ComicsService, private store: Store<AppState>) {
+    this.items$ = this.store.select(selectComics).pipe(
+      map((state: ComicsState) => state.characters)
+    );
+  }
+
+  ngOnInit(): void {
+    this.comicsService.getComics().subscribe((data: ListItemsModel[]) => {
+      console.log(data);
+      this.store.dispatch(loadComicsSuccess({ comics: data }));
+    });
+  }
 }
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ListItemComponent } from '../components/ListItems/ListItems';
+import { StoreModule } from '@ngrx/store';
+import { comicsReducer } from '../store/reducers/comics';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    HttpClientModule,
+    StoreModule.forRoot({ comics: comicsReducer })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
